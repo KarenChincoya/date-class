fefd456c2ee8020331eaecccf72f52b77de66429
KarenChincoya <30363201+KarenChincoya@users.noreply.github.com>
2018-02-11 17:38:42 -0600

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,18 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fecha f = new Fecha();
+		f.setDia(5);
+		f.setMes(5);
+		f.setAnio(2016);
+		
+		f.aumentarUnDia();
+		f.disminuirUnDia();
+		f.aumentarNDias(5);
+		f.disminuirNDias(3);
+		
+	}
+
+}
